743fb5e06ea3559a365a6381753bf0901b62e8f2
mattzab <mattzab@gmail.com>
2017-07-22 18:43:52 -0600

    Update Download_All.sh

--- Download_All.sh
+++ Download_All.sh
@@ -2,11 +2,11 @@
 echo Starting Script...
 echo  
 echo Cleaning up XML files to be used with Wget...
-
-echo replace </title> with .mp3
+echo  
+echo replace trailing title tag with mp3 file extension
 sed -i -- 's,</Title>,.mp3,g' *.xml
 
-echo remove ! from titles
+echo remove exclamation points from titles
 sed -i -- 's,!,,g' *.xml
 
 #remove all ( from titles
@@ -15,16 +15,16 @@ sed -i -- 's,!,,g' *.xml
 #remove all ) from titles
 #sed -i 's,),,g' *.xml
 
-echo remove ' from titles
+echo remove single quotes from titles
 sed -i "s,',,g" *.xml
 
-echo remove &amp; from titles
+echo remove ampersand from titles
 sed -i 's,&amp;,And,g' *.xml
 
-echo replace : with . in verses
+echo replace colon with period in verses
 sed -i -- 's,:,.,g' *.xml
 
-echo restore the : in http://
+echo restore the colon in url addresses
 sed -i -- 's,p./,p:/,g' *.xml
 
 echo remove spaces from titles
@@ -36,24 +36,24 @@ sed -i -- 's,<Title>,,g' *.xml
 echo remove leading url tag
 sed -i -- 's,<Url>,,g' *.xml
 
-echo remove all remaining < and > tags and everything in-between
+echo remove all remaining xml tags and everything inbetween
 sed -i 's,<.*>,,g' *.xml
 
 echo cleanup white space
 sed -i '/^$/d' *.xml
 
-echo make a directory for all files
+echo make a directory for all xml files and copy xml files into corresponding directory
 find . -name "*.xml" -exec sh -c 'mkdir "${1%.*}" ; mv "$1" "${1%.*}" ' _ {} \;
 
-
-
-echo wget everything oneliner
+echo download everything by using wget
 for d in ./*/ ; do (cd "$d" && while read filename; do read url; wget -O $filename $url; done < *.xml); done
 
 echo delete xml files
 for d in ./*/ ; do (cd "$d" && rm *.xml); done
 
 #tell the user the process is finally over
+echo  
+echo  
 echo *******THANK YOU*******
 echo  
 echo Thank you for your patience.
