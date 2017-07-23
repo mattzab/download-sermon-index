527d1cc14ba8f17ff238ee4d3acc9a659e5f05b9
mattzab <mattzab@gmail.com>
2017-07-22 19:26:02 -0600

    Create OSX_Downloader.sh

---
+++ OSX_Downloader.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo Make Sermon Index Directory
+mkdir SermonIndex
+cd SermonIndex
+echo  
+echo Download updated Speaker Listing
+curl -o cache.zip http://img.sermonindex.net/Cache_Update.zip
+echo Extract Speaker Listing File
+echo  
+unzip *.zip
+echo Delete Speaker Listing Zip
+echo  
+rm *.zip
+echo  
+echo Cleaning up XML files to be used with Curl...
+echo  
+echo replace trailing title tag with mp3 file extension
+sed -i -- 's,</Title>,.mp3,g' *.xml
+echo 
+echo remove exclamation points from titles
+sed -i -- 's,!,,g' *.xml
+echo 
+echo remove single quotes from titles
+sed -i "s,',,g" *.xml
+echo 
+echo remove ampersand from titles
+sed -i 's,&amp;,And,g' *.xml
+echo 
+echo replace colon with period in verses
+sed -i -- 's,:,.,g' *.xml
+echo 
+echo restore the colon in url addresses
+sed -i -- 's,p./,p:/,g' *.xml
+echo 
+echo remove spaces from titles
+sed -i -- 's, ,,g' *.xml
+echo 
+echo remove leading title tag
+sed -i -- 's,<Title>,,g' *.xml
+echo 
+echo remove leading url tag
+sed -i -- 's,<Url>,,g' *.xml
+echo 
+echo remove all remaining xml tags and everything inbetween
+sed -i 's,<.*>,,g' *.xml
+echo 
+echo cleanup white space
+sed -i '/^$/d' *.xml
+echo 
+echo make a directory for all xml files and copy xml files into corresponding directory
+find . -name "*.xml" -exec sh -c 'mkdir "${1%.*}" ; mv "$1" "${1%.*}" ' _ {} \;
+echo 
+echo download everything by using curl
+for d in ./*/ ; do (cd "$d" && while read filename; do read url; curl -Lo $filename $url; done < *.xml); done
+echo 
+echo delete xml files
+for d in ./*/ ; do (cd "$d" && rm *.xml); done
+echo 
+#tell the user the process is finally over
+echo  
+echo  
+echo *******THANK YOU*******
+echo  
+echo Thank you for your patience.
+echo The download has finally finished.
+echo  
